9c16b3fe4028279fa085b5eab8eee78a21048699
Tom Fahy <thomas.fahy9@gmail.com>
2023-11-10 11:26:15 +0000

    add js prompts for pw character options

--- script.js
+++ script.js
@@ -90,12 +90,29 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+
   let pw_length = parseInt(prompt("How long should your password be? (enter a number between 8 and 128): "));
+
   while ((pw_length > 128) || (pw_length < 8)) {
     alert("Your password length must be between 8 and 128 characters. Please try again. ");
     pw_length = parseInt(prompt("How long should your password be? (enter a number between 8 and 128): "));
   }
+
+  let character_options = [];
+
+  character_options.push(prompt("Should the password use lower-case characters? Y/N: "));
+  character_options.push(prompt("Should the password use upper-case characters? Y/N: ")); 
+  character_options.push(prompt("Should the password use numerals? Y/N: "));
+  character_options.push(prompt("Should the password use special characters ? Y/N: "));
   
+  // check for nonsense Input
+  // check at least one character type has been selected
+
+  let password_options = {length: pw_length, char_options: character_options};
+
+  return password_options;  
+
+
 }
 
 // Function for getting a random element from an array
