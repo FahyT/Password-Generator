6a640c8a8bf4dc2ebc53893609606f32257d0789
Tom Fahy <thomas.fahy9@gmail.com>
2023-11-10 11:53:06 +0000

    refactor password options function to use object

--- script.js
+++ script.js
@@ -91,27 +91,24 @@ var upperCasedCharacters = [
 // Function to prompt user for password options
 function getPasswordOptions() {
 
-  let pw_length = parseInt(prompt("How long should your password be? (enter a number between 8 and 128): "));
+  let password_options = {};
 
-  while ((pw_length > 128) || (pw_length < 8)) {
+  password_options.length = parseInt(prompt("How long should your password be? (enter a number between 8 and 128): "));
+
+  while ((password_options.length > 128) || (password_options.length < 8)) {
     alert("Your password length must be between 8 and 128 characters. Please try again. ");
     pw_length = parseInt(prompt("How long should your password be? (enter a number between 8 and 128): "));
   }
 
-  let character_options = [];
-
-  character_options.push(prompt("Should the password use lower-case characters? Y/N: "));
-  character_options.push(prompt("Should the password use upper-case characters? Y/N: ")); 
-  character_options.push(prompt("Should the password use numerals? Y/N: "));
-  character_options.push(prompt("Should the password use special characters ? Y/N: "));
+  password_options.pw_lowercase = prompt("Should the password use lower-case characters? Y/N: ");
+  password_options.pw_uppercase = prompt("Should the password use upper-case characters? Y/N: "); 
+  password_options.pw_numerals =prompt("Should the password use numerals? Y/N: ");
+  password_options.pw_specials =prompt("Should the password use special characters ? Y/N: ");
   
   // check for nonsense Input
   // check at least one character type has been selected
 
-  let password_options = {length: pw_length, char_options: character_options};
-
-  return password_options;  
-
+  return password_options;
 
 }
 
@@ -121,7 +118,7 @@ function getRandom(arr) {
 }
 
 // Function to generate password with user input
-function generatePassword() {
+function generatePassword(password_options) {
 
 }
 
